abb6d898d6d5e94ac25ff95eed17a1e1bc8013a7
geoffrey jost <geoffrey@jostwebwerks.com>
2018-03-21 17:56:38 -0700

    entity,segment: make jsonload_topics more tolerant of bad data

--- repo_models/entity.py
+++ repo_models/entity.py
@@ -1072,7 +1072,8 @@ def TEMP_scrub_topicdata(data):
                 item['id'] = tid
                 item['term'] = TEMP_this.TOPICS[tid]
             logging.debug('    -> %s' % item)
-        item['term'] = TEMP_this.TOPICS[item['id']]
+        if TEMP_this.TOPICS.get(item['id']):
+            item['term'] = TEMP_this.TOPICS[item['id']]
     return data
 def jsonload_topics(text):
     return TEMP_scrub_topicdata(
